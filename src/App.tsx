ed2981cd4441dff36df1736a215694b0b542b48b
Vishwa Rajapaksha <vishwarajapaksha16@gmail.com>
2025-10-19 14:35:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// src/App.tsx
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-3xl font-bold">MazeRunner Project</h1>
+    </div>
+  )
+}
+
+export default App
